6ae758dd9f5efe8d79b1d2a796dfa0b14756e7c1
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2022-03-26 22:31:02 -0300

    provider aws-eks

---
+++ terraforms/modules/network/variables.tf
@@ -0,0 +1,4 @@
+locals {
+  cluster_name = "personalerp-cluster-${var.tags["Env"]}"
+}
+variable "tags" { }
---
+++ terraforms/prd/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = "0.12.20"
+  
+  backend "s3" {
+    region="sa-east-1"
+    profile="personalerp"
+    bucket="personalerp-aws-admin"
+    key="personalerp-infra-state-stg"
+  }
+}
+
+provider "aws" {
+  region  = "sa-east-1"
+  profile = "personalerp"
+  version = "~> 2.52"
+}
+
+provider "tls" {
+  version = "~> 2.1"
+}
+
+module "network" {
+  source = "../modules/network"
+  tags = {
+    Env = basename(path.cwd)
+  }
+}
+
+module "cluster" {
+  source         = "../modules/cluster"
+  vpc_id         = module.network.vpc_id
+  eks_subnet_ids = module.network.eks_subnet_ids
+  cluster_name   = module.network.cluster_name
+
+  tags = {
+    Env = basename(path.cwd)
+  }
+}
+
+module "worknodes" {
+  source         = "../modules/worknodes"
+  eks_subnet_ids = module.network.eks_subnet_ids
+  cluster_name   = module.network.cluster_name
+
+  worknodes = 1
+  worknode_desired_size = 2
+  worknode_max_size = 2
+  worknode_min_size = 2
+
+  tags = {
+    Env = basename(path.cwd)
+  }
+}
